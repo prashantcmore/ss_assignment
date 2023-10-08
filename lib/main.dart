382315989e59b1dc2b279a641b0c1470388f073b
Prashant More <prashantmore8698@gmail.com>
2023-10-08 16:07:54 +0530

    first commit

--- lib/main.dart
+++ lib/main.dart
@@ -30,7 +30,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.red,
       ),
       debugShowCheckedModeBanner: false,
-      home: const LoginScreen(),
+      home: const SplashScreen(),
       
    ),);
        
